f5dcfb489fa5dae867240cbaca38b3645d98d026
limborock12 <82361240+limborock12@users.noreply.github.com>
2024-10-20 21:34:16 +0800

    Add files via upload

--- build/models/purchaseorder.js
+++ build/models/purchaseorder.js
@@ -1,4 +1,20 @@
 const mongoose = require('mongoose');
+<<<<<<< HEAD
+const Schema = mongoose.Schema;
+
+const PurchaseOrderSchema = new Schema({
+    supplier: { type: String, required: true },
+    item: {
+        itemName: { type: String, required: true }, 
+        quantity: { type: Number, required: true },
+        cost: { type: Number, required: true },
+        branchStored: {type: String, required: true}
+    },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('PurchaseOrder', PurchaseOrderSchema);
+=======
 
 const purchaseOrderSchema = new mongoose.Schema({
     orderNumber: { type: String, required: true },
@@ -11,3 +27,4 @@ const purchaseOrderSchema = new mongoose.Schema({
 const PurchaseOrder = mongoose.model('PurchaseOrder', purchaseOrderSchema);
 
 module.exports = PurchaseOrder;
+>>>>>>> 9f1041c9cab39ac7f1acaa76d5eac5c07d43d882
