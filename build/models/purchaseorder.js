d3520bfe94bab9b6bf3c93094cc5c8eae2d60aec
limborock12 <82361240+limborock12@users.noreply.github.com>
2024-10-20 18:35:04 +0800

    created purchase order schema

---
+++ build/models/purchaseorder.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PurchaseOrderSchema = new Schema({
+    supplier: { type: String, required: true },
+    item: {
+        itemId: { type: Schema.Types.ObjectId, ref: 'Item', required: true },
+        quantity: { type: Number, required: true },
+        cost: { type: Number, required: true }
+    },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('PurchaseOrder', PurchaseOrderSchema);
